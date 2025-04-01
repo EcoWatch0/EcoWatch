7a2433c06fde51cb464e8bb4a7864694abde74c8
louis sasse <louis.sasse@ynov.com>
2025-04-01 19:43:22 +0200

    chore: Update ESLint configuration and dependencies
    - Refactored ESLint configuration to include prettier integration and updated ignore patterns.
    - Upgraded TypeScript ESLint packages to version 8.29.0 and ESLint packages to their latest versions.
    - Removed deprecated rules and adjusted existing ones for better compatibility.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,33 +3,19 @@ import eslint from '@eslint/js';
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
+import eslintConfigPrettier from 'eslint-config-prettier';
 
 export default tseslint.config(
-  {
-    ignores: ['eslint.config.mjs'],
-  },
   eslint.configs.recommended,
-  ...tseslint.configs.recommendedTypeChecked,
-  eslintPluginPrettierRecommended,
+  ...tseslint.configs.recommended,
+  eslintConfigPrettier,
   {
-    languageOptions: {
-      globals: {
-        ...globals.node,
-        ...globals.jest,
-      },
-      ecmaVersion: 5,
-      sourceType: 'module',
-      parserOptions: {
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname,
-      },
-    },
-  },
-  {
+    ignores: ['**/dist/**', '**/node_modules/**'],
     rules: {
+      '@typescript-eslint/interface-name-prefix': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
       '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
     },
   },
 );
