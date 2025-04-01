817a611362ca1101e7c8aa1e7ec2efc44b3309ee
louis sasse <louis.sasse@ynov.com>
2025-04-01 20:16:52 +0200

    chore: Update dependencies and add Swagger documentation for EcoWatch API
    - Upgraded reflect-metadata to version 0.2.2 across the project for improved compatibility.
    - Added @nestjs/swagger dependency to enable API documentation.
    - Configured Swagger in the main application file and added API documentation annotations in the AppController.
    - Updated README.md to reflect project structure and installation instructions in French.

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { INestApplication } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app: INestApplication = await NestFactory.create(AppModule);
+
+  // Configuration Swagger
+  const config = new DocumentBuilder()
+    .setTitle('EcoWatch API')
+    .setDescription('Documentation de l\'API EcoWatch')
+    .setVersion('1.0')
+    .addTag('ecowatch')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap(); 
